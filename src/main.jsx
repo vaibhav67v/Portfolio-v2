8ed9e4103e3705df44f2a3e8631d67bda82d3d2a
Vaibhav Vishwakarma <vishwakarmavaibhav2002@gmail.com>
2026-03-25 01:43:55 +0530

    Setup project structure with routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,22 @@
-import { createRoot } from "react-dom/client";
-import App from "./App"
+import {createRoot} from "react-dom/client";
+import {createBrowserRouter} from "react-router";
+import {RouterProvider} from "react-router/dom";
+import App from "./App";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
+    },
+]);
 
 const root = createRoot(document.querySelector("#root"));
 
-root.render(<App />)
+root.render(<RouterProvider router={router} />);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Hero from "../sections/Hero";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
+import Contact from "../sections/Contact";
+
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <About />
+            <Projects/>
+            <Contact />
+        </>
+    );
+}
