4229794cc2d5dc8616d22a5eda8c7b8a396c7323
Vaibhav Vishwakarma <vishwakarmavaibhav2002@gmail.com>
2026-04-18 13:23:53 +0530

    Set the default font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Contact from "../sections/Contact";
 export default function Home() {
 	const primaryBgClr = "bg-neutral-900";
 	return (
-		<div className={`${primaryBgClr} min-h-screen`}>
+		<div className={`${primaryBgClr} min-h-screen font-['Space_Grotesk']`}>
 			<Hero />
 			<About />
 			<Projects/>
